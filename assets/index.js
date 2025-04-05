245688bd55e6674c50be10365313b269a3cc2b01
psgnezdilov <psoshenko8@gmail.com>
2025-04-05 12:39:43 +0100

    Form validity check

--- assets/index.js
+++ assets/index.js
@@ -1,14 +1,22 @@
-const form = document.getElementById('myForm');
-form.addEventListener('submit', function(event) {
-    event.preventDefault();
-    form.reset();
-});
-const toastTrigger = document.getElementById('ToastBtn')
-const toastLiveExample = document.getElementById('Toast')
+(() => {
+    'use strict';
 
-if (toastTrigger) {
-    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
-    toastTrigger.addEventListener('click', () => {
-    toastBootstrap.show()
-})
-}
+    const form = document.getElementById('myForm');
+    const toastLiveExample = document.getElementById('Toast');
+    const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample);
+
+    if (form) {
+        form.addEventListener('submit', event => {
+            event.preventDefault();
+            event.stopPropagation();
+
+            if (!form.checkValidity()) {
+            } else {
+                form.reset();
+                form.classList.remove('was-validated');
+                toastBootstrap.show();
+            }
+            form.classList.add('was-validated');
+        }, false);
+    }
+})();
